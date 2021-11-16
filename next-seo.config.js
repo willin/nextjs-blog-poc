186e5d4a72c08638ab1ceb91fbf9daff2249f8c8
Willin Wang <willin@willin.org>
2021-11-16 17:48:17 +0800

    chore: :construction: add some packages

---
+++ next-seo.config.js
@@ -0,0 +1,19 @@
+const NextSeo = {
+  defaultTitle: 'Willin Wang',
+  titleTemplate: '%s - Willin Wang',
+  description: 'To be Willin is to be willing.',
+
+  openGraph: {
+    type: 'website',
+    url: 'https://willin.wang',
+    site_name: 'Willin Wang'
+  },
+
+  twitter: {
+    handle: '@willinwang',
+    site: '@willinwang',
+    cardType: 'summary_large_image'
+  }
+};
+
+export default NextSeo;
