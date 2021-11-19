ea62d4c4351fbaaa9bef3e140cf26941393f9d7c
Willin Wang <willin@willin.org>
2021-11-19 16:01:44 +0800

    style: :recycle: update tailwind style

--- next.config.mjs
+++ next.config.mjs
@@ -7,17 +7,17 @@ export default withContentlayer()({
   i18n: {
     locales: ['en', 'zh'],
     defaultLocale: 'en',
-    localeDetection: false
-  },
-  webpack: (config, { dev, isServer }) => {
-    // Replace React with Preact only in client production build
-    if (!dev && !isServer) {
-      Object.assign(config.resolve.alias, {
-        react: 'preact/compat',
-        'react-dom/test-utils': 'preact/test-utils',
-        'react-dom': 'preact/compat'
-      });
-    }
-    return config;
+    localeDetection: true
   }
+  // webpack: (config, { dev, isServer }) => {
+  //   // Replace React with Preact only in client production build
+  //   if (!dev && !isServer) {
+  //     Object.assign(config.resolve.alias, {
+  //       react: 'preact/compat',
+  //       'react-dom/test-utils': 'preact/test-utils',
+  //       'react-dom': 'preact/compat'
+  //     });
+  //   }
+  //   return config;
+  // }
 });
